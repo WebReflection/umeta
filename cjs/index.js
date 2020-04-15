7880e13947d31b09bfc056b9b45cceef9e1fd248
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-04-15 11:54:49 +0200

    ordered alphabetically

--- cjs/index.js
+++ cjs/index.js
@@ -1,10 +1,12 @@
 'use strict';
 const {createRequire} = require('module');
-const path = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('path'));
 const {fileURLToPath} = require('url');
+const path = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('path'));
+
+const {dirname} = path;
 
 module.exports = ({url}) => {
   const fileName = fileURLToPath(url);
-  const dirName = path.dirname(fileName);
+  const dirName = dirname(fileName);
   return {fileName, dirName, require: createRequire(url)};
 };
--- esm/index.js
+++ esm/index.js
@@ -1,9 +1,11 @@
 import {createRequire} from 'module';
-import path from 'path';
 import {fileURLToPath} from 'url';
+import path from 'path';
+
+const {dirname} = path;
 
 export default ({url}) => {
   const fileName = fileURLToPath(url);
-  const dirName = path.dirname(fileName);
+  const dirName = dirname(fileName);
   return {fileName, dirName, require: createRequire(url)};
 };
